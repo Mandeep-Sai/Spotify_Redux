934a11cb5b4ba5a0b5857ceaed6f62c58fdd944c
Mandeep Sai <63707885+Mandeep-Sai@users.noreply.github.com>
2020-07-31 14:04:16 +0200

    username to redux added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,22 +5,29 @@ export default function (state = {}, action) {
         ...state,
         selectedSong: action.payload,
       };
-      case "LIKED_SONG":
+    case "LIKED_SONG":
       return {
         ...state,
         likedSong: true,
-        listOfLikedSongs: state.listOfLikedSongs.concat(action.payload)
+        listOfLikedSongs: state.listOfLikedSongs.concat(action.payload),
       };
-      case "UNLIKED_SONG":
-        const songToRemove = state.listOfLikedSongs.findIndex(name => name === action.payload)
-        return {
-          ...state,
-          likedSong: false,
-          listOfLikedSongs: [
-            ...state.listOfLikedSongs.slice(0, songToRemove),
-            ...state.listOfLikedSongs.slice(songToRemove + 1)
-          ]
-        };
+    case "UNLIKED_SONG":
+      const songToRemove = state.listOfLikedSongs.findIndex(
+        (name) => name === action.payload
+      );
+      return {
+        ...state,
+        likedSong: false,
+        listOfLikedSongs: [
+          ...state.listOfLikedSongs.slice(0, songToRemove),
+          ...state.listOfLikedSongs.slice(songToRemove + 1),
+        ],
+      };
+    case "LOAD_USER":
+      return {
+        ...state,
+        username: action.payload,
+      };
     default:
       return state;
   }
